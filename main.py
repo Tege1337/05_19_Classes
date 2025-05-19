836736add5df3221d48ffc5f3abe913d1bb12d3c
Tege1337 <toth-nador.tege@diak.szbi-pg.hu>
2025-05-19 09:32:29 +0200

    tivadar info

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ class Student:
 
 tivadar = Student()
 print(tivadar)
+
+tivadar.name = "El Tivadar"
+tivadar.age = 16
+tivadar.sex = "Attack Helicopter"
+tivadar.score = 20
+
+print(f"\nNév: {tivadar.name} \nKor: {tivadar.age} \nPontszám: {tivadar.score}")
